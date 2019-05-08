21c460a477c7f9f85ce07ae4895bcb7f310c84ce
Alzahraa-Ahmed <alzahraa.ahmed55@gmail.com>
2019-05-08 16:47:55 +0200

    Define AST static attributes - Define ASTNode attribute and abstract generate method

--- gen/AST.java
+++ gen/AST.java
@@ -1,52 +1,85 @@
+import java.util.*;
+
 public class AST {
 
-    public static class ASTNode{
+    public static ArrayList<String> threeAddressCode = new ArrayList<>();
+    public static int tCounter = 1; //counter for temp variables
+    public static int lCounter = 1; // counter for labels
+
+    public static abstract class ASTNode {
+        int lineNo;
+
+        abstract void generate();
 
     }
 
-    static class Program extends ASTNode{}
-    static class Block extends ASTNode{
-        public Block() {
-            System.out.println("block");
+    static class Program extends ASTNode {
+    }
+
+    static class Block extends ASTNode {
+    }
+
+    static class Feature extends Block {
+    }
+
+    static class Method extends Feature {
+    }
+
+    static class Property extends Feature {
+    }
+
+    static class Formal extends ASTNode {
+    }
+
+
+    //    Productions of expr
+    static class Expression extends ASTNode {
+    }
+
+    static class Assignment extends Expression {
+    }
+
+    static class If extends Expression {
+    }
+
+    static class While extends Expression {
+    }
+
+    static class BlockOfExpressions extends Expression {
+    }
+
+    static class Let extends Expression {
+    }
+
+    static class Case extends Expression {
+    }
+
+    static class NewType extends Expression {
+    }
+
+    static class IsVoid extends Expression {
+    }
+
+    static class Arithmetic extends Expression {
+    }
+
+    static class Relational extends Expression {
+    }
+
+    static class Logic extends Expression {
+    } //Not
+
+    static class Id extends Expression {
+    }
+
+    static class Int extends Expression {
+    }
+
+    static class Literal extends Expression {
+    }
+
+    static class Bool extends Expression {
+        public Bool(Boolean value) {
         }
     }
-
-    static class Feature extends Block{}
-
-   static class Method extends Feature{}
-
-   static class Property extends Feature{}
-
-   static class Formal extends ASTNode{}
-
-
-//    Productions of expr
-   static class Expression extends ASTNode{}
-
-   static class Assignment extends Expression{}
-
-   static class If extends Expression{}
-
-   static class While extends Expression{}
-
-   static class BlockOfExpressions extends Expression{}
-
-   static class Let extends Expression{}
-
-   static class Case extends Expression{}
-
-   static class NewType extends Expression{}
-
-   static class IsVoid extends Expression{}
-
-   static class Arithmetic extends Expression{}
-   static class Relational extends Expression{}
-   static class Logic extends Expression{} //Not
-   static class Id extends Expression{}
-   static class Int extends Expression{}
-   static class Literal extends Expression{}
-   static class Bool extends Expression{
-       public Bool(Boolean value) {
-       }
-   }
 }
